3402bec3240d4340d08c2ca9cc84191a20d08455
Robert Zenz <Robert.Zenz@bonsaimind.org>
2017-06-21 23:05:15 +0200

    Added processing of 0b, 0o and 0x prefixes.

--- src/org/bonsaimind/jmathpaper/Evaluator.java
+++ src/org/bonsaimind/jmathpaper/Evaluator.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,7 +30,10 @@ import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class Evaluator {
+	private static final Pattern BINARY_NUMBER = Pattern.compile("(^|[^0-9])0b(?<VALUE>[01]+)($|[^.,])");
+	private static final Pattern HEX_NUMBER = Pattern.compile("(^|[^0-9])0x(?<VALUE>[0-9a-fA-F]+)($|[^.,])");
 	private static final Pattern ID_FINDER = Pattern.compile("^(?<ID>[a-zA-Z_]+)=(?<EXPRESSION>.*)$");
+	private static final Pattern OCTAL_NUMBER = Pattern.compile("(^|[^0-9])0o(?<VALUE>[0-7]+)($|[^.,])");
 	private List<EvaluatedExpression> evaluatedExpressions = new ArrayList<>();
 	private Map<String, Double> variables = new HashMap<>();
 	
@@ -41,6 +45,18 @@ public class Evaluator {
 		
 		addVariable("e", Math.E);
 		addVariable("E", Math.E);
+	}
+	
+	private static final String convertFromBinary(String value) {
+		return Long.toString(Long.parseLong(value, 2));
+	}
+	
+	private static final String convertFromHex(String value) {
+		return Long.toString(Long.parseLong(value, 16));
+	}
+	
+	private static final String convertFromOctal(String value) {
+		return Long.toString(Long.parseLong(value, 8));
 	}
 	
 	public void addVariable(String name, double value) {
@@ -87,7 +103,24 @@ public class Evaluator {
 		variables.remove(name);
 	}
 	
+	private String applyPattern(String expression, Pattern pattern, Function<String, String> replacer) {
+		StringBuffer buffer = new StringBuffer(expression.length());
+		Matcher matcher = pattern.matcher(expression);
+		
+		while (matcher.find()) {
+			matcher.appendReplacement(buffer, replacer.apply(matcher.group("VALUE")));
+		}
+		
+		matcher.appendTail(buffer);
+		
+		return buffer.toString();
+	}
+	
 	private double evaluateInternal(String expression) {
+		expression = applyPattern(expression, BINARY_NUMBER, Evaluator::convertFromBinary);
+		expression = applyPattern(expression, OCTAL_NUMBER, Evaluator::convertFromOctal);
+		expression = applyPattern(expression, HEX_NUMBER, Evaluator::convertFromHex);
+		
 		String processedExpression = expression.replace('#', 'R');
 		
 		ExpressionBuilder expressionBuilder = new ExpressionBuilder(processedExpression);
