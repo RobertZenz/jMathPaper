f7e2cfd8b7cda6b44de328e4dfa3bfbfa220c50d
Robert Zenz <Robert.Zenz@bonsaimind.org>
2017-09-20 16:45:32 +0200

    Added license headers and whitespace correction.

--- src/org/bonsaimind/jmathpaper/cli/Cli.java
+++ src/org/bonsaimind/jmathpaper/cli/Cli.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017, Robert 'Bobby' Zenz
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.bonsaimind.jmathpaper.cli;
 
@@ -6,7 +22,6 @@ import org.bonsaimind.jmathpaper.core.EvaluatedExpression;
 import org.bonsaimind.jmathpaper.core.Evaluator;
 
 public final class Cli {
-	
 	private Cli() {
 		super();
 	}
--- src/org/bonsaimind/jmathpaper/swt/Swt.java
+++ src/org/bonsaimind/jmathpaper/swt/Swt.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017, Robert 'Bobby' Zenz
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.bonsaimind.jmathpaper.swt;
 
@@ -9,7 +25,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public final class Swt {
-	
 	private Swt() {
 		super();
 	}
