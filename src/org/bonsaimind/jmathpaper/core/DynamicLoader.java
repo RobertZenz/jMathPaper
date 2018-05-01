21b6bda0e55c76144d9207f99e5c1f7e36ef00b5
Robert Zenz <Robert.Zenz@bonsaimind.org>
2018-05-01 11:05:33 +0200

    Fixed copyright year.

--- src/org/bonsaimind/jmathpaper/core/DynamicLoader.java
+++ src/org/bonsaimind/jmathpaper/core/DynamicLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, Robert 'Bobby' Zenz
+ * Copyright 2018, Robert 'Bobby' Zenz
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
